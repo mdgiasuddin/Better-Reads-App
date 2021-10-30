93fe2fc27f7a43b0402507622590157c4f650954
Md. Giash Uddin <mdgiashu069@gmail.com>
2021-10-30 14:30:12 +0600

    Model and repository added

--- src/main/java/io/javabrains/connection/DataStaxAstraProperties.java
+++ src/main/java/io/javabrains/connection/DataStaxAstraProperties.java
@@ -7,7 +7,6 @@ import java.io.File;
 @ConfigurationProperties(prefix = "datastax.astra")
 public class DataStaxAstraProperties {
     private File secureConnectBundle;
-
     public void setSecureConnectBundle(File secureConnectBundle) {
         this.secureConnectBundle = secureConnectBundle;
     }
